c137097e8ee612cb47f40d2f9913e71b7b432e36
user2410 <dc241024@gmail.com>
2023-01-27 14:54:51 +0700

    Transition to Book

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	before_action :setup_header_vars
+
+	private
+		def setup_header_vars
+			@signed_in = user_signed_in?
+			if @signed_in
+				@user = current_user
+			end
+		end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @signed_in = user_signed_in?
-    if @signed_in
-      @user = current_user
-    end
   end
 end
